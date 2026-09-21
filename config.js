c7bdb8aa9c1ac14cc6b06499cd890d0d0018c37a
7979hzd4gj-oss <7979hzd4gj@privaterelay.appleid.com>
2026-09-21 13:26:27 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,45 @@
 import 'dotenv/config';
-const _prefixes = process.env.PREFIXES ? process.env.PREFIXES.split(',') : ['.', '!', '/', '#'];
+
+const _prefixes = process.env.PREFIXES? process.env.PREFIXES.split(',') : ['.', '!', '/', '🍫'];
+
 const config = {
-    // Bot Identity
-    botName: process.env.BOT_NAME || 'MEGA-MD',
-    botOwner: process.env.BOT_OWNER || 'Qasim Ali',
-    ownerNumber: process.env.OWNER_NUMBER || '923051391007',
-    author: process.env.AUTHOR || 'GlobalTechInfo',
-    packname: process.env.PACKNAME || 'MEGA-MD',
-    description: process.env.DESCRIPTION || 'High performance multi-device WhatsApp bot',
-    version: '6.0.0',
+    // Bot Identity - CHOCO
+    botName: process.env.BOT_NAME || 'CHOCO-ITACHI-V2',
+    botOwner: process.env.BOT_OWNER || 'CHOCO ITACHI',
+    ownerNumber: process.env.OWNER_NUMBER || '224000000000',
+    author: process.env.AUTHOR || 'CHOCO-ITACHI',
+    packname: process.env.PACKNAME || 'CHOCO-ITACHI-V2 😈',
+    description: process.env.DESCRIPTION || '🍫 CHOCO-ITACHI-V2 - Le bot WhatsApp le plus violent 😈',
+    version: '2.0.0',
+
     // Bot Config
     prefixes: _prefixes,
     prefix: _prefixes[0],
     commandMode: process.env.COMMAND_MODE || 'public',
-    timeZone: process.env.TIMEZONE || 'Asia/Karachi',
-    // Links
+    timeZone: process.env.TIMEZONE || 'Africa/Conakry',
+
+    // Links CHOCO
     channelLink: process.env.CHANNEL_LINK || 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
-    updateZipUrl: process.env.UPDATE_URL || 'https://github.com/GlobalTechInfo/MEGA-MD/archive/refs/heads/main.zip',
-    ytChannel: process.env.YT_CHANNEL || 'GlobalTechInfo',
+    updateZipUrl: process.env.UPDATE_URL || 'https://github.com/CHOCO-ITACHI/CHOCO-ITACHI-V2/archive/refs/heads/main.zip',
+    ytChannel: process.env.YT_CHANNEL || 'CHOCO ITACHI',
+
     // Session
     sessionId: process.env.SESSION_ID || '',
     pairingNumber: process.env.PAIRING_NUMBER || '',
+
     // Performance
     port: Number(process.env.PORT) || 5000,
-    maxStoreMessages: Number(process.env.MAX_STORE_MESSAGES) || 20,
+    maxStoreMessages: Number(process.env.MAX_STORE_MESSAGES) || 50,
     tempCleanupInterval: Number(process.env.CLEANUP_INTERVAL) || 1 * 60 * 60 * 1000,
     storeWriteInterval: Number(process.env.STORE_WRITE_INTERVAL) || 10000,
+
     // API Keys
     giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
     removeBgKey: process.env.REMOVEBG_KEY || '',
-    // Warn system
+
+    // Warn system - CHOCO
     warnCount: 3,
+
     // External APIs
     APIs: {
         xteam: 'https://api.xteam.xyz',
@@ -53,4 +62,5 @@ const config = {
         'https://api-fgmods.ddns.net': 'fg-dylux'
     }
 };
+
 export default config;
